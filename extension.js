e46d70a5cb8908482fd5a0ce94b369139dc23229
Daniel Falk <danielfalk22@gmail.com>
2021-02-17 16:31:22 -0500

    Gnome 3.38
    Changes:
    - Remove Tweener import.  It's not used and was causing an error in 3.38

--- extension.js
+++ extension.js
@@ -1,6 +1,5 @@
 const St = imports.gi.St;
 const Main = imports.ui.main;
-const Tweener = imports.ui.tweener;
 
 const Me = imports.misc.extensionUtils.getCurrentExtension();
 const Indicator = Me.imports.breaktimerIndicator.Indicator;
